01aaf2baf915e5eb9cd2b19909754fd2796e913f
Glory42 <gorkemkaryol03@gmail.com>
2025-02-19 19:43:41 +0300

    Updated user routes: unnecessary functions moved.

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const userRouter = express.Router();
+const jwt = require('jsonwebtoken');
+const users = require('../config/db');
+const { getAllUsers, userRegister, userLogin } = require('../controllers/userController');
 
-userRouter.get('/', (req, res) => {
+const SECRET_KEY = process.env.JWT_SECRET;
 
-});
+userRouter.get('/', getAllUsers);
+
+userRouter.post('/register', userRegister);
+
+userRouter.post('/login', userLogin);
+
+module.exports = userRouter;
