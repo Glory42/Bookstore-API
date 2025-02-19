a7dcb5ef742a06e72d3d55ea600eef6136067c30
Glory42 <gorkemkaryol03@gmail.com>
2025-02-19 19:35:11 +0300

    Updated book routes: unnecessary functions moved.

--- routes/order.js
+++ routes/order.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const orderRouter = express.Router();
+const authMiddleware = require('../middleware/auth');
+const { getAllOrder, createOrder, getUserOrder } = require('../controllers/orderController');
+const orders = require('../config/db');
 
-orderRouter.get('/', (req, res) => {
+orderRouter.get('/', authMiddleware, getAllOrder);
 
-});
+orderRouter.get('/my-orders', authMiddleware, getUserOrder);
+
+orderRouter.post('/', authMiddleware, createOrder);
+
+
+module.exports = orderRouter;
